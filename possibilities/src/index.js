1f29a3a08d8fad940830a314f5986ae60270264f
Adetunji <ashennaike@gmail.com>
2019-04-15 13:02:42 -0400

    added the initial setup for the index src file, will work on the aesthetic of the app page once i get work from the UI team about which design they picked. then waiting on the data structure for figuring out how to import it and use it correctly with react charts

--- possibilities/src/index.js
+++ possibilities/src/index.js
@@ -4,7 +4,29 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//import reducer
+// import rootReducer from './reducers'
+
+//provider import, along with store and middleware
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+
+import thunk from 'redux-thunk'
+import logger from 'redux-logger'
+
+//apply middleware inside of the store as well as the reducer
+const store = createStore(
+  // rootReducer,
+  applyMiddleware( thunk, logger )
+)
+
+const rootElement = document.getElementById('root')
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App /> 
+  </Provider>, 
+rootElement);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
