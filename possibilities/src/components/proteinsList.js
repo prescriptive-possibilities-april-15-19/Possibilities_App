648ede53ba043442bf1c09f5083065d9480d85ea
Adetunji <ashennaike@gmail.com>
2019-04-17 18:30:37 -0400

    smile/protein componenets and list components are done, going to do the search components for each and then merge

--- possibilities/src/components/proteinsList.js
+++ possibilities/src/components/proteinsList.js
@@ -1,1 +1,26 @@
 //map through the data to make a list of individual protein items
+import React from "react";
+
+//import protein component
+import { Protein } from "./";
+
+//import links to send the user to individual protein pages using the id
+import { Link } from 'react-router-dom'
+
+//import styled components
+import styled from 'styled-components'
+
+
+
+const ProteinList = props => {
+  console.log(props)
+  return (
+    <ul>
+      {props.proteins.map(protein => {
+        return <Protein key={protein.id} protein={protein} />;
+      })}
+    </ul>
+  );
+};
+
+export default ProteinList;
