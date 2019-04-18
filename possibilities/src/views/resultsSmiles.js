4f13c5e6f83eb0702387d1240bd1c714aa20a949
Adetunji <ashennaike@gmail.com>
2019-04-17 20:41:10 -0400

    added a data reducer, will make more for the other actions and put them through a reducer combiner to export from a single file

--- possibilities/src/views/resultsSmiles.js
+++ possibilities/src/views/resultsSmiles.js
@@ -6,7 +6,8 @@ import { connect } from 'react-redux'
 //import styled components 
 import styled from 'styled-components'
 
-
+//import components that will be used
+import { SmilesList, SmilesSearch } from '../components'
 
 //display all of the results for the search
 class ResultsSmiles extends React.Component {
