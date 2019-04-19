6ebff2f775dcd72db7446ddcfcf6ad29308c3e7b
Adetunji <ashennaike@gmail.com>
2019-04-18 20:36:23 -0400

    fixed the animation completely for styling and view

--- possibilities/src/components/visual.js
+++ possibilities/src/components/visual.js
@@ -1,44 +1,3 @@
-
-// // // Create NGL Stage object
-// // var stage = new NGL.Stage( "viewport" );
-
-// // // Handle window resizing
-
-
-
-// // // Load PDB entry 1CRN
-// // stage.loadFile( "rcsb://1crn", { defaultRepresentation: true } );
-
-
-// import React, { Component } from 'react';
-// import { Stage } from 'ngl';
-
-// class MoleculeViewer extends Component {  
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       stage: new Stage("viewport")
-//     }
-//   }
-
-//   componentDidMount() {
-//     this.state.stage.loadFile( "rcsb://1crn", { defaultRepresentation: true });
-//     window.addEventListener( "resize", function( event ){
-//       this.state.stage.handleResize();
-//     }, false );
-//   }
-  
-//   render() {
-//     return (
-//       <div id="viewport" style={{width: "100%"}}>
-//      </div>
-//     );
-//   }
-// }
-
-// export default MoleculeViewer;
-
-
 import React, { Component } from 'react';
 import { Stage } from 'ngl';
 
@@ -54,9 +13,24 @@ class MoleculeViewer extends Component {
   }
 
   componentDidMount() {
-    var stage = new Stage("viewport");
-    setTimeout( () => stage.loadFile(this.props.url, {ext: this.props.ext, defaultRepresentation: true}) );
-    this.setState({stage});
+    var stage = new Stage( "viewport", {backgroundColor: "white"} );
+      stage.spinAnimation.axis = {x: 0, y: 0.5, z: 1.0};
+      // stage.mouseControls.disabled = true;
+      stage.mouseControls.clear();
+      // stage.animationControls.pause(true);
+      stage.setSpin(true);
+    stage.loadFile(this.state.url, {ext: this.props.ext})
+      .then( o => {
+        o.addRepresentation( "cartoon", {color: "skyblue"} );
+        o.addRepresentation( "ball+stick", {sele: "protein and (sidechain or .CA)", color: "purple"});
+        o.addRepresentation( "licorice", {
+            sele: "not protein",
+            color: "red",
+            radius: 0.5
+        });
+        o.autoView();
+      })
+    // this.setState({stage});
   }
 
   componentWillUnmount() {
@@ -65,7 +39,7 @@ class MoleculeViewer extends Component {
   
   render() {
     return (
-      <div id="viewport" style={{width: "100%"}}>
+      <div id="viewport" style={{height: '600px',width: "100%"}}>
         <img src={this.state.loadingImg} id="loading-img" className="Loading-Img" alt="loading" style = {{margin: "auto", display: this.state.url ? "none" : "block" }} />
       </div>
     );
